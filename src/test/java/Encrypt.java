524d3d8a54cee9ee4a5784d5ce86f601ab08df3b
josphatmwania <josphat.muyo@student.moringaschool.com>
2021-08-01 19:10:38 +0300

    Add encrypt test method

--- src/test/java/Encrypt.java
+++ src/test/java/Encrypt.java
@@ -3,8 +3,11 @@ import org.junit.Test;
 public class Encrypt {
     @Test
     public void checkWhetherInputIsString() {
+        Encrypt classUnderTest = new Encrypt();
+        assertNotNull("app should convert a c when shift is 2", classUnderTest.encryptClass("A", 2));
 
-        //
+
+
 
     }
 }
