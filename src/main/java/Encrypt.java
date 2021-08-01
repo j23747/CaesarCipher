a074d84f5714209cdf503ba9eb88ec59d272c915
josphatmwania <josphat.muyo@student.moringaschool.com>
2021-08-01 19:59:28 +0300

    modify Encrypt method

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -12,5 +12,22 @@ public class Encrypt {
         } else  if (shift < 0) {
             shift = (shift % 26) + 26;
         }
+
+        for ( int i = 0; i < message.length(); i++) {
+            if (Character.isUpperCase(message.charAt(i))) {
+                char character = (char) (((int) message.charAt(i) +
+                        shift - 65) % 26 + 65);
+                encryptedResult.append(character);
+
+            } else  {
+                char character = (char) (((int) message.charAt(i) +
+                        shift - 97) % 26 + 97);
+                encryptedResult.append(character);
+            }
+        }
+        return  encryptedResult;
+
+
+
     }
 }
